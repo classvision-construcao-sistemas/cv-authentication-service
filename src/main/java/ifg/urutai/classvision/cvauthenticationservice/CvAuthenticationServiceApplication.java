28ae776c50011b2d75da1dfcf35880784fbca4d4
Pedro Cesar Rocha <pedrocesar.prof@gmail.com>
2026-09-04 09:18:54 -0300

    feat: Commit inicial

---
+++ src/main/java/ifg/urutai/classvision/cvauthenticationservice/CvAuthenticationServiceApplication.java
@@ -0,0 +1,13 @@
+package ifg.urutai.classvision.cvauthenticationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CvAuthenticationServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CvAuthenticationServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ifg/urutai/classvision/cvauthenticationservice/CvAuthenticationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ifg.urutai.classvision.cvauthenticationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CvAuthenticationServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
